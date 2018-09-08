3a3fedf90ecf5584bfef3c98b8d3e29e001b3211
id github: malaysia21 <agnieszka.puzia@gmail.com>
2018-09-08 17:48:16 +0200

    Implement Spring Config for Services. Closes #22

--- pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package aga.spring.sfgpetclinic.services.map;
 
 import aga.spring.sfgpetclinic.model.Owner;
 import aga.spring.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
 
     @Override
--- pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/services/map/VetServiceMap.java
@@ -2,9 +2,11 @@ package aga.spring.sfgpetclinic.services.map;
 
 import aga.spring.sfgpetclinic.model.Vet;
 import aga.spring.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     @Override
