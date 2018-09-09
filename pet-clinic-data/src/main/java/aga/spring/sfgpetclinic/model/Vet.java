bf2bae2d60598b4c42af628634deedde9942dd62
id github: malaysia21 <agnieszka.puzia@gmail.com>
2018-09-09 11:40:54 +0200

    Create Vat Speciality Entity, Associate to Vet. Closes #27

--- pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/model/Vet.java
@@ -1,4 +1,14 @@
 package aga.spring.sfgpetclinic.model;
 
 public class Vet extends Person {
+
+    private Speciality speciality;
+
+    public Speciality getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(Speciality speciality) {
+        this.speciality = speciality;
+    }
 }
