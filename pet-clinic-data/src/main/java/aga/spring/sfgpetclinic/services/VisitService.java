b57113e0f3a4dd9e75ff0f0be152b6c09dccdb5e
id github: malaysia21 <agnieszka.puzia@gmail.com>
2018-09-10 19:20:23 +0200

    Create Visit Map Service. Closes #46

---
+++ pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package aga.spring.sfgpetclinic.services;
+
+import aga.spring.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/aga/spring/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,39 @@
+package aga.spring.sfgpetclinic.services.map;
+
+import aga.spring.sfgpetclinic.model.Visit;
+import aga.spring.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+     @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if(object.getPet() == null || object.getPet().getOwner() == null || object.getPet().getId()==null){
+          throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
